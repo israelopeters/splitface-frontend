7428d14ba047e5acbd0109ad5396f15d6e334a32
israelopeters <israelopeters@gmail.com>
2024-07-01 13:49:09 +0100

    Feat: Implement RecyclerView for post layout on landing page

--- app/src/main/java/com/northcoders/tatooine/ui/main/MainActivity.java
+++ app/src/main/java/com/northcoders/tatooine/ui/main/MainActivity.java
@@ -6,16 +6,43 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import com.northcoders.tatooine.R;
 import com.northcoders.tatooine.ui.login.LoginActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.widget.GridLayout.HORIZONTAL;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerViewLayout);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new GridLayoutManager(this, 2, RecyclerView.VERTICAL, false);
+//        layoutManager = new StaggeredGridLayoutManager(2, RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ArrayList<String> testPriceList = new ArrayList<>();
+        for (int i = 25; i < 100; i++) {
+            testPriceList.add("Price" + i);
+        }
+
+        adapter = new PostAdapter(testPriceList);
+        recyclerView.setAdapter(adapter);
 
     }
 }
---
+++ app/src/main/java/com/northcoders/tatooine/ui/main/PostAdapter.java
@@ -0,0 +1,74 @@
+package com.northcoders.tatooine.ui.main;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.northcoders.tatooine.R;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder> {
+
+    private ArrayList<String> priceList;
+
+    public PostAdapter(ArrayList<String> priceList) {
+        this.priceList = priceList;
+    }
+
+    public class PostViewHolder extends RecyclerView.ViewHolder {
+
+        TextView priceHolder;
+//        TextView timeTakenHolder;
+        View layout;
+
+        public PostViewHolder(@NonNull @NotNull View postView) {
+            super(postView);
+            layout = postView;
+            priceHolder = (TextView) postView.findViewById(R.id.priceTextView);
+//            timeTakenHolder = (TextView) postView.findViewById(R.id.timeTakenTextView);
+        }
+    }
+
+    public void add(int position, String item) {
+        priceList.add(position, item);
+        notifyItemInserted(position);
+    }
+
+    public void remove(int position) {
+        priceList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+
+    @NonNull
+    @NotNull
+    @Override
+    public PostAdapter.PostViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.activity_post_layout, parent, false);
+        PostViewHolder postViewHolder = new PostViewHolder(view);
+        return postViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull PostViewHolder postViewHolder, @SuppressLint("RecyclerView") int position) {
+        final String price = priceList.get(position);
+        postViewHolder.priceHolder.setText(price);
+        postViewHolder.priceHolder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                remove(position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return priceList.size();
+    }
+}
