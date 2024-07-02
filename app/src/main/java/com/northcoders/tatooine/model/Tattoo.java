d52364d4ac6bde4eabde0575cec921fabdb092be
Jackson MacDonald <jacksonmacdonald@gmail.com>
2024-07-02 11:44:52 +0100

    Feat: BaseObservable annotations added to Tattoo class

--- app/src/main/java/com/northcoders/tatooine/model/Tattoo.java
+++ app/src/main/java/com/northcoders/tatooine/model/Tattoo.java
@@ -1,10 +1,14 @@
 package com.northcoders.tatooine.model;
 
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
 import com.google.gson.annotations.SerializedName;
+import com.northcoders.tatooine.BR;
 
 import java.util.List;
 
-public class Tattoo {
+public class Tattoo extends BaseObservable {
     @SerializedName("id")
     private Long id;
 
@@ -35,51 +39,63 @@ public class Tattoo {
         this.styles = styles;
     }
 
+    @Bindable
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+        notifyPropertyChanged(BR.id);
     }
 
+    @Bindable
     public List<Style> getStyles() {
         return styles;
     }
 
     public void setStyles(List<Style> styles) {
         this.styles = styles;
+        notifyPropertyChanged(BR.styles);
     }
 
+    @Bindable
     public Artist getArtist() {
         return artist;
     }
 
     public void setArtist(Artist artist) {
         this.artist = artist;
+        notifyPropertyChanged(BR.artist);
     }
 
+    @Bindable
     public String getPrice() {
         return price;
     }
 
     public void setPrice(String price) {
         this.price = price;
+        notifyPropertyChanged(BR.price);
     }
 
+    @Bindable
     public String getDesign() {
         return design;
     }
 
     public void setDesign(String design) {
         this.design = design;
+        notifyPropertyChanged(BR.design);
     }
 
+    @Bindable
     public String getTimeTaken() {
         return timeTaken;
     }
 
     public void setTimeTaken(String timeTaken) {
         this.timeTaken = timeTaken;
+        notifyPropertyChanged(BR.timeTaken);
     }
 }
