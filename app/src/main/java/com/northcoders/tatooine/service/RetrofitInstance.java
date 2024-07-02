2fa95cb9c1bc0375c4505012f84409282a6fe7e3
Jackson MacDonald <jacksonmacdonald@gmail.com>
2024-07-02 10:47:48 +0100

    Feat: Added in retrofit instance and tattoo service

---
+++ app/src/main/java/com/northcoders/tatooine/service/RetrofitInstance.java
@@ -0,0 +1,19 @@
+package com.northcoders.tatooine.service;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+    private static RetrofitInstance retrofitInstance = null;
+    private static final String BASE_URL = "/"; // add base URL
+
+    public static TattooAPIService getService(){
+
+        if (retrofitInstance == null){
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+    }
+}
---
+++ app/src/main/java/com/northcoders/tatooine/service/TattooAPIService.java
@@ -0,0 +1,14 @@
+package com.northcoders.tatooine.service;
+
+import com.northcoders.tatooine.model.Tattoo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface TattooAPIService {
+    @GET("") // add path
+    Call<List<Tattoo>> getUser(@Path("username") String username); // change path
+}
