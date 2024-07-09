a56682a31c626d266f005d2902ba4d3c82dc7288
Jackson MacDonald <jacksonmacdonald@gmail.com>
2024-07-09 14:46:54 +0100

    Feat: View model created for UpdateProfile

---
+++ app/src/main/java/com/northcoders/tatooine/ui/updateprofile/UpdateProfileViewModel.java
@@ -0,0 +1,22 @@
+package com.northcoders.tatooine.ui.updateprofile;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.northcoders.tatooine.model.Artist;
+import com.northcoders.tatooine.model.ArtistRepository;
+
+public class UpdateProfileViewModel extends AndroidViewModel {
+    private ArtistRepository artistRepository;
+
+    public UpdateProfileViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public LiveData<Artist> editArtistProfile(Long id, Artist artist){
+        return artistRepository.editArtistById(id, artist);
+    }
+}
